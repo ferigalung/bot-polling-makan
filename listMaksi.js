00306379f6e2fb4c73a40a2c1261808a465e941c
Wahyu Akbar Wibowo <wahyuwibowoworker@gmail.com>
2023-10-26 03:59:37 +0000

    increase menu makanan

--- listMaksi.js
+++ listMaksi.js
@@ -1,6 +1,6 @@
 const listMaksi = [
-  ["Mamamia", "Nyengitz", "Je'O", "JnD"],
-  ["Milinesia", "Lava", "Shinta", "Yola"],
+  ["Mamamia", "Nyengitz", "Je'O", "JnD", "Milinesia", "Lava", "Shinta", "Yola", "Bebek Elien"],
+  ["Milinesia", "Lava", "Shinta", "Yola", "Mamamia", "Nyengitz", "Je'O", "JnD", "Bebek Elien"],
 ];
 
 module.exports = listMaksi;
